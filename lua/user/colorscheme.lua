ae4ee949356108c7df5c5d30deeb911988efdca8
Vinicius Araujo <vinicius.ar02@gmail.com>
2022-05-25 18:15:59 -0300

    first commit

---
+++ lua/user/colorscheme.lua
@@ -0,0 +1,14 @@
+local colorscheme = "tokyonight"
+
+vim.g.tokyonight_transparent = true
+-- vim.g.material_style = "palenight"
+
+require('material').setup({
+  lualine_style = 'stealth'  
+})
+
+local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
+if not status_ok then
+    vim.notify("colorscheme " .. colorscheme .. " not found!")
+    return
+end
---
+++ lua/user/lualine.lua
@@ -0,0 +1,54 @@
+local status_ok, lualine = pcall(require, "lualine")
+if not status_ok then 
+  return
+end
+
+local diff = {
+	"diff",
+	colored = false,
+	symbols = { added = "  ", modified = " ", removed = " " },
+	cond = hide_in_width,
+}
+
+local diagnostics = {
+	"diagnostics",
+	sources = { "nvim_diagnostic" },
+	sections = { "error", "warn" },
+	symbols = { error = " ", warn = " " },
+	colored = true,
+	update_in_insert = false,
+	always_visible = true,
+}
+
+lualine.setup {
+  options = {
+    -- globalstatus = true,
+    theme = 'auto',
+    disabled_filetypes = { "alpha", "dashboard", "Outline" },
+		always_divide_middle = true,
+    component_separators = '|',
+    section_separators = { left = '', right = '' },
+  },
+  sections = {
+    lualine_a = {
+      { 'mode', separator = { left = '' }, right_padding = 2 },
+    },
+    lualine_b = { 'filename', 'branch' },
+    lualine_c = { 'fileformat' },
+    lualine_x = {},
+    lualine_y = { 'filetype', 'progress' },
+    lualine_z = {
+      { 'location', separator = { right = '' }, left_padding = 2 },
+    },
+  },
+  inactive_sections = {
+    lualine_a = { 'filename' },
+    lualine_b = {},
+    lualine_c = {},
+    lualine_x = {},
+    lualine_y = {},
+    lualine_z = { 'location' },
+  },
+  tabline = {},
+  extensions = {},
+}
